91f5623c897b74d7a1225c73bcbc5925e71dcf2e
lucasgfeldmann <lucasgfeldmann@gmail.com>
2026-04-07 18:49:01 -0300

    using arrays and maps

--- ex-0001/hello/hello.go
+++ ex-0001/hello/hello.go
@@ -10,9 +10,12 @@ import (
 func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-	message, err := greetings.Hello("")
+
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(message)
+	fmt.Println(messages)
 }
